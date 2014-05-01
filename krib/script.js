987c7fc5dd08a38b35634cf1b557a37448e82590
Philipp Koschinski <philipp.koschinski@googlemail.com>
2014-05-01 22:29:47 +0200

    Clean up script
    Removed unused functions left over from AnnaRave

--- krib/script.js
+++ krib/script.js
@@ -1,33 +1,5 @@
 "use strict";
 
-// Here we can specify all available songs
-var songs = [
-    {
-        "name": "1",
-        "url": "http://frozen.kuschku.de/annarave/res/fysb.mp3"
-    },
-    {
-        "name": "2",
-        "url": "http://frozen.kuschku.de/annarave/res/lig.mp3"
-    },
-    {
-        "name": "3",
-        "url": "http://frozen.kuschku.de/annarave/res/harlemshake.mp3"
-    },
-    {
-        "name": "4",
-        "url": "http://frozen.kuschku.de/annarave/res/giraffes.mp3"
-    },
-    {
-        "name": "5",
-        "url": "http://frozen.kuschku.de/annarave/res/atomicink.mp3"
-    },
-    {
-        "name": "6",
-        "url": "http://frozen.kuschku.de/annarave/res/butterfly.mp3"
-    }
-];
-
 if (typeof console === "undefined") {
     window.console = {
         log: function () {}
@@ -71,20 +43,6 @@ function forEachOfClass(classname, callback) {
     }
 }
 
-// stops all songs at once
-function pauseAll() {
-    console.log("pauseAll();");
-
-    forEachOfClass("song", function (entry) {
-        // pause the song
-        entry.pause();
-        try {
-            // reset current position to start
-            entry.currentTime = 0;
-        } catch (e) {}
-    });
-}
-
 // set volume to a value between 0 and 1 by setting it for each song
 function setVolume(value) {
     console.log("setVolume(" + value.toString() + ");");
@@ -121,33 +79,6 @@ function play(song) {
     window.location.hash = (song + 1).toString();
 }
 
-// This creates the buttons and the audio elements for our songs
-function createSongs() {
-    console.log("createSongs();");
-
-    var parent = document.getElementById("songs"), i, song, mutebtn;
-    // for each song out of our list we create each one button and one audio element
-    for (i = 0; i < songs.length; i += 1) {
-        song = document.createElement("span");
-        song.innerHTML = '<a class="button" href="#' + (i + 1).toString() + '">' + songs[i].name + '</a>';
-        song.innerHTML += '<audio loop id="' + i.toString() + '" volume="0.5" class="song" src="' + songs[i].url + '"></audio>';
-        // we append these now to the main parent
-        parent.appendChild(song);
-    }
-}
-
-// Determines which song to play, uses local data of last song and the URL
-function getInitialSong() {
-    console.log("getInitialSong();");
-
-    var song = 0, urlsong = window.location.hash.substring(1);
-
-    if (urlsong > 0 && urlsong < songs.length + 1) {
-        song = urlsong - 1;
-    }
-    return song;
-}
-
 function initVolume() {
     var volume = 50;
     if (getItem("volume") !== null && getItem("volume") !== undefined) {
@@ -161,20 +92,11 @@ function initVolume() {
 function init() {
     console.log("init();");
 
-    // We create buttons and audio objects for each song
-    //createSongs();
-
     // we load the volume from the slider and apply it (some browsers store slider values)
     addListener(document.getElementById("volume"), "change", changeVolume);
     
     // Load volume from localstorage
     initVolume();
-    
-    // now we start the first song
-    //play(getInitialSong());
-    
-    // Add a listener for a URL change
-    //addListener(window, "hashchange", function () { play(getInitialSong()); });
 }
 
 addListener(window, "load", init);
