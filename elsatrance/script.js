1978f0e9afc7bd1fabf79e54047f0cb19f8a513b
Philipp Koschinski <philipp.koschinski@googlemail.com>
2014-05-06 21:57:12 +0200

    Added possibility to jump directly to a specific song

--- elsatrance/script.js
+++ elsatrance/script.js
@@ -1,4 +1,14 @@
+"use strict";
+
+if (typeof console === "undefined") {
+    window.console = {
+        log: function () {}
+    };
+}
+
 var quality = 1;
+
+var widget;
 
 var srcList = [
     "lq",
@@ -8,11 +18,32 @@ var srcList = [
 
 var qualSelector;
 
+// Adds a listener independent of browser
+function addListener(object, event, callback) {
+    console.log("addListener(" + object + "," + event + "," + callback.name + ");");
+
+    if (object.addEventListener) {
+        object.addEventListener(event, callback, false); //W3C
+    } else {
+        object.attachEvent('on' + event, callback); //IE
+    }
+}
+
+
+// Iterates through all elements of this class
+function forEachOfClass(classname, callback) {
+    console.log("forEachOfClass(" + classname + "," + callback.name + ");");
+
+    var item, iterator, elements = document.getElementsByClassName(classname);
+    for (iterator = 0; iterator < elements.length; iterator += 1) {
+        callback(elements[iterator]);
+    }
+}
+
 function updateQuality() {
-    var video = document.querySelectorAll("video.video")[0];
-    var type = video.currentSrc.substr(video.currentSrc.lastIndexOf("."));
-    if (type!="mp4" && type != "webm") {
-        type = "webm"
+    var video = document.querySelectorAll("video.video")[0], type = video.currentSrc.substr(video.currentSrc.lastIndexOf("."));
+    if (type !== "mp4" && type !== "webm") {
+        type = "webm";
     }
     video.src = "res/" + srcList[quality] + "." + type;
     video.play();
@@ -24,7 +55,7 @@ function speedtest(callback) {
     download.onload = function () {
         endTime = (new Date()).getTime();
         var duration = (endTime - startTime) / 1000, bitsLoaded = downloadSize * 8, speedBps = (bitsLoaded / duration).toFixed(2), speedKbps = (speedBps / 1024).toFixed(2), speedMbps = (speedKbps / 1024).toFixed(2);
-        callback(speedMbps)
+        callback(speedMbps);
     };
     startTime = (new Date()).getTime();
     download.src = imageAddr;
@@ -39,29 +70,49 @@ function selectQualityBySpeed(speed) {
     updateQuality();
 }
 
-$(document).ready(function () {
-    var widget = SC.Widget(document.getElementById('soundcloud_widget'));
+function skipToSong(number) {
+    widget.skip(number);
+}
+
+function getInitialSong() {
+    var urlsong = 1;
+    urlsong = window.location.hash.substring(1);
+    return urlsong - 1;
+}
+
+function init() {
+    widget = SC.Widget(document.getElementById('soundcloud_widget'));
     widget.bind(SC.Widget.Events.READY, function () {
-        console.log('Ready...');
+        skipToSong(getInitialSong());
     });
-    $("#play").click(function () {
+
+    addListener(document.getElementById("play"), "click", function () {
         widget.toggle();
     });
-    $("#volume").click(function () {
+
+    addListener(document.getElementById("volume"), "click", function () {
         widget.setVolume(document.getElementById("volume").value);
     });
-    $("#volume").change(function () {
+
+    addListener(document.getElementById("volume"), "change", function () {
         widget.setVolume(document.getElementById("volume").value);
     });
-    $("#scloud").click(function () {
-        $("#soundcloud_widget").toggleClass("visible");
+
+    addListener(document.getElementById("scloud"), "click", function () {
+        document.getElementById("soundcloud_widget").classList.toggle("visible");
     });
-    $("#quality").change(function () {
+
+    addListener(document.getElementById("quality"), "change", function () {
         quality = this.value;
-        changeQuality();
+        updateQuality();
     });
+
     document.getElementById("quality").selectedIndex = 1;
     widget.setVolume(document.getElementById("volume").value);
     speedtest(selectQualityBySpeed);
     updateQuality();
-});
+
+    addListener(window, "hashchange", function () { skipToSong(getInitialSong()); });
+}
+
+addListener(window, "load", init);
